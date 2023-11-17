96528555af1fad17ae21fb324a556699a51f9042
nhantran3395 <thanhnhan33.95@gmail.com>
2023-11-17 21:11:09 +0700

    Isolate each test with new store

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,18 +1,31 @@
-import { render, screen, within } from 'test/utilities';
-import PackingList from '.';
+import { ReactElement } from "react";
+import { Provider } from "react-redux";
 import userEvent from "@testing-library/user-event";
 
+import { render, screen, within } from 'test/utilities';
+import { PackingList } from '.';
+
+import { createStore } from "./store";
+
+const renderWithStoreProvider = (ui : ReactElement) => {
+    return render(
+        <Provider store={createStore()}>
+            {ui}
+        </Provider>
+    );
+};
+
 it('renders the Packing List application', () => {
-  render(<PackingList />);
+  renderWithStoreProvider(<PackingList />);
 });
 
 it('has the correct title', async () => {
-  render(<PackingList />);
+  renderWithStoreProvider(<PackingList />);
   screen.getByText('Packing List');
 });
 
 it('has an input field for a new item', () => {
-  render(<PackingList />);
+  renderWithStoreProvider(<PackingList />);
   expect(screen.getByRole('searchbox', { name: /new item/i})).toBeInTheDocument();
   expect(screen.getByRole('button', { name: /add new item/i})).toBeInTheDocument();
 });
@@ -20,7 +33,7 @@ it('has an input field for a new item', () => {
 it(
   'has a "Add New Item" button that is disabled when the input is empty',
   () => {
-    render(<PackingList />);
+    renderWithStoreProvider(<PackingList />);
     expect(screen.getByRole('button', { name: /add new item/i})).toBeDisabled();
   },
 );
@@ -29,7 +42,7 @@ it(
   'enables the "Add New Item" button when there is text in the input field',
   async () => {
       const user = userEvent.setup();
-      render(<PackingList />);
+      renderWithStoreProvider(<PackingList />);
       await user.type(screen.getByRole('searchbox', { name: /new item/i}), 'iPhone');
       expect(screen.getByRole('button', { name: /add new item/i})).not.toBeDisabled();
   },
@@ -39,7 +52,7 @@ it(
   'adds a new item to the unpacked item list when the clicking "Add New Item"',
   async () => {
       const user = userEvent.setup();
-      render(<PackingList />);
+      renderWithStoreProvider(<PackingList />);
       await user.type(screen.getByRole('searchbox', { name: /new item/i}), 'iPhone');
       await user.click(screen.getByRole('button', { name: /add new item/i}));
       expect(within(screen.getByTestId('unpacked-items')).getByLabelText('iPhone')).not.toBeChecked();
