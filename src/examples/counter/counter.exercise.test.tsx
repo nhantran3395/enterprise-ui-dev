16eb56d21070d723d0053c544fbc2a44e4ed4248
nhantran3395 <thanhnhan33.95@gmail.com>
2023-11-17 20:02:49 +0700

    Add test for counter

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -20,9 +20,17 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={5}/>);
+  expect(screen.getByTestId('current-count')).toHaveTextContent('5');
+});
 
-test.todo(
+test(
   'it should reset the count when the "Reset" button is pressed',
-  async () => {},
+  async () => {
+    const user = userEvent.setup();
+    render(<Counter initialCount={10}/>);
+    await user.click(screen.getByRole('button', { name: /reset/i}));
+    expect(screen.getByTestId('current-count')).toHaveTextContent('0');
+  },
 );
