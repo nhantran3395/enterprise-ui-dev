e353cf72a2a2a6686c9f4373169184bf5925f433
nhantran3395 <thanhnhan33.95@gmail.com>
2023-11-17 20:31:42 +0700

    Add test for packing list

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from 'test/utilities';
+import { render, screen, within } from 'test/utilities';
 import PackingList from '.';
+import userEvent from "@testing-library/user-event";
 
 it('renders the Packing List application', () => {
   render(<PackingList />);
@@ -10,19 +11,37 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  expect(screen.getByRole('searchbox', { name: /new item/i})).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /add new item/i})).toBeInTheDocument();
+});
 
-it.todo(
+it(
   'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
+  () => {
+    render(<PackingList />);
+    expect(screen.getByRole('button', { name: /add new item/i})).toBeDisabled();
+  },
 );
 
-it.todo(
+it(
   'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
+  async () => {
+      const user = userEvent.setup();
+      render(<PackingList />);
+      await user.type(screen.getByRole('searchbox', { name: /new item/i}), 'iPhone');
+      expect(screen.getByRole('button', { name: /add new item/i})).not.toBeDisabled();
+  },
 );
 
-it.todo(
+it(
   'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
+  async () => {
+      const user = userEvent.setup();
+      render(<PackingList />);
+      await user.type(screen.getByRole('searchbox', { name: /new item/i}), 'iPhone');
+      await user.click(screen.getByRole('button', { name: /add new item/i}));
+      expect(within(screen.getByTestId('unpacked-items')).getByLabelText('iPhone')).not.toBeChecked();
+  },
 );
